83f11f444cd5d207749e0769ffb11c8edf1494ee
Rohith Ravula <ravularohit45@gmail.com>
2026-01-27 12:33:30 +0530

    Lists_1

---
+++ Lists_python.py
@@ -0,0 +1,22 @@
+"""N="Rohith"
+A=29
+Pe=91.2
+Stu=["Rohith",29,91.2]
+print(type(Stu))
+print(Stu)
+print(Stu[0])"""
+
+DOW=["Mon","Tue","Wed","Thur","Fri","Sat","Sun"]
+print(type(DOW))
+# Indexing in Lists
+print(DOW[4])
+print(DOW[-2])
+print(f"The first weekend of the day is {DOW[-2]}")
+# Length of a List
+print(len(DOW))
+print(f"There are {len(DOW)} days in the week")
+# Slicing in a List
+print(DOW[0:6:1])
+print(DOW[1:6:2])
+print(DOW[0:8:1])
+print(DOW[0:8:2])
